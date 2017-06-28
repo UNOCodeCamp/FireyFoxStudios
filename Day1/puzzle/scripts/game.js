c206b9c34f1568ac779dd6819598a572b6d3d43a
firey fox studios <fireyfoxstudios@gmail.com>
2017-06-28 14:05:46 +0000

    Day 2 commit Firey Fox

--- Day1/puzzle/scripts/game.js
+++ Day1/puzzle/scripts/game.js
@@ -1,43 +1,50 @@
 var passcode= ~~(Math.random()*1000);
 var tries = 10;
+
+var clueText = document.getElementById("blues-clues");
+
+    var button = document.getElementById("guess-button");
+    var number = document.getElementById("guess-number");
+
+var attemptsText =document.getElementById("tries");
+
+
+button.addEventListener("click", guessNumber);
+
+function guessNumber()
 {
-    console.log("You have " +tries+ " guesses left.");
-    tries = tries - 1;
-    console.log("Guess a number between 0 and 999");
-    var guess = prompt("Enter a number between (0-999)");
-    tries = tries-1;
+        tries = tries - 1;
+    attemptsText.innerHTML = ("Number of Attempts left: " + tries);
+    var guess = number.value;
+    //var guess = prompt("Enter a number (0 - 999")")"
+
+
     if (guess == passcode)
     {
-     console.log("you win");
-     alert("You Win!!");
+        document.body.innerHTML = "<h1>You Win!</h1>";
+
     }
     else if (tries <= 0)
     {
-        console.log("you lose");
-        alert("You Lose... You suck at this game. ");
+        document.body.innerHTML = "<h1>You lose! LOL</h1>";
+    
     }
     else {
-        giveClue();
+        giveClue(guess);
     }
 }
 
-function giveClue()
+function giveClue(guess)
 {
+
+    
     if(guess > passcode)
     {
-        console.log("Guess is too high, try again.");
+        clueText.innerHTML += "<li>" + guess + " is too high!" + "</li>";
     }
     else
     {
-        console.log("Guess is too low, try again.");
+         clueText.innerHTML += "<li>" + guess + " is too low!" + "</li>";
     }
     
 }
-
-
-
-
-
-
-
-
