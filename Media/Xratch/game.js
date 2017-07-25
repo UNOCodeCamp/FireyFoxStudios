e4cead0a9171df8f19cc13647d52327961b277be
Chase Lirette <chaselirette@yahoo.com>
2017-07-25 15:51:50 -0500

    repush

--- Media/Xratch/game.js
+++
@@ -1,7 +0,0 @@
-var canvas = document.getElementById("viewport");
-var ctx = canvas.getContext("2d");
-
-var scene = new Image();
-scene.src = "Firey Fox Background.gf";
-
-ctx.drawImage(scene,0,0,  500, 500);
